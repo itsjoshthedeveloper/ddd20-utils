961fad1805bfd7ddab2f539ae73c756f7850dc98
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-04-23 16:40:09 -0400

    Sync frame selection for start in seconds

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ import numpy as np
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('filename')
-    p.add_argument('--start',               default=0,      type=int,   help='Start index')
+    p.add_argument('--start',               default=0,      type=float, help='Start secs')
     p.add_argument('--length',              default=10,     type=int,   help='Length of window')
     p.add_argument("--bsizes",              default=None,   type=float, help='Bin sizes', nargs='+')
     p.add_argument('--mode',                default='plot', type=str,   help='Mode', choices=['plot','analyze'])
@@ -26,29 +26,33 @@ if __name__ == '__main__':
         if binsize in args.bsizes:
             with h5py.File(path, 'r') as f:
                 data = f['dvs_accum'].value
-                print(binsize, data.shape)
+                if args.mode == 'analyze':
+                    print(binsize, data.shape)
+                else:
+                    print(binsize, end=' ', flush=True)
                 export_data[binsize] = data
+    print()
 
-    indexes = range(args.start, args.start + args.length)
-
-    cols = len(indexes)
+    cols = args.length
     rows = len(export_data)
 
     for row, (binsize, data) in enumerate(export_data.items()):
-        for col in indexes:
-            ax = plt.subplot(rows, cols, (col-args.start)+1+(row*cols))
+        start = int(args.start / binsize)
+        indexes = range(start, start + args.length)
+
+        for i in indexes:
+            ax = plt.subplot(rows, cols, (i-start)+1+(row*cols))
             plt.xticks([], [])
             plt.yticks([], [])
             
-            if row == 0:
-                ax.set_title('frame {}'.format(col))
-            if (col-args.start) == 0:
+            ax.set_title('frame {}'.format(i))
+            if (i-start) == 0:
                 plt.ylabel("{}s".format(binsize))
 
-            img = data[col]
+            img = data[i]
 
             if args.mode == 'analyze':
-                print(binsize, col, img.shape, np.unique(img), np.mean(img))
+                print(binsize, i, img.shape, np.unique(img), np.mean(img))
             
             ax.imshow(img, cmap='gray')
 
