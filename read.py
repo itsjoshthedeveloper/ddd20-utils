956b8148c7550d25a3f35f78518e3ea8903d4a2a
Josh Chough <34778271+itsjoshthedeveloper@users.noreply.github.com>
2021-05-06 00:14:44 -0400

    Add ability to read hdf5 files

---
+++ read.py
@@ -0,0 +1,18 @@
+import h5py
+import argparse
+import numpy as np
+from tqdm import tqdm
+import os
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('path',                              type=str,   help='Path of recording')
+    args = p.parse_args()
+
+    print('opening ' + args.path)
+    with h5py.File(args.path, 'r') as f:
+
+        def printinfo(name):
+            print(name, f[name])
+
+        f.visit(printinfo)
